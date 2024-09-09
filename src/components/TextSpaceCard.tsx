2ea7da69b37f066f23fe046fd1f5f87c0f8fb741
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-09-09 17:00:14 +0100

    Made component responsive and modified the options design

--- src/components/TextSpaceCard.tsx
+++ src/components/TextSpaceCard.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import { BsDot } from "react-icons/bs";
-import { MdMoreHoriz, MdPerson, MdClose, MdOutlineDelete, MdArrowForward } from "react-icons/md";
+import { MdMoreHoriz, MdPerson, MdClose, MdOutlineDelete, MdArrowForward, MdOutlineFavorite } from "react-icons/md";
 import { FiCopy } from "react-icons/fi";
 
 const OPTIONS = [
@@ -21,6 +21,13 @@ const OPTIONS = [
         Icon: FiCopy
     },
     {
+        title: "Add to favorite",
+        action: "favorite",
+        hoverBackground: "hover:bg-purple-100",
+        hoverColor: "hover:text-purple-600",
+        Icon: MdOutlineFavorite
+    },
+    {
         title: "Delete space",
         action: "delete",
         hoverBackground: "hover:bg-red-100",
@@ -30,7 +37,6 @@ const OPTIONS = [
 ]
 
 export default function TextSpaceCard({ id, index = 0 }: { id: number | string; index?: number; }) {
-    const navigate = useNavigate();
     const [showOptions, setShowOptions] = useState(false);
     const [isCloserToLeft, setIsCloserToLeft] = useState(false);
 
@@ -45,9 +51,9 @@ export default function TextSpaceCard({ id, index = 0 }: { id: number | string; 
     }
 
     return (
-        <div className={`relative ${showOptions ? 'before:opacity-100 z-10' : 'before:opacity-0 before:pointer-events-none'} before:transition-opacity before:duration-300 before:fixed before:top-0 before:left-0 before:w-full before:h-full before:backdrop-blur-sm before:bg-black/10`}>
+        <div className={`relative ${showOptions ? 'before:opacity-100 z-10' : 'before:opacity-0 before:pointer-events-none'} before:transition-opacity before:duration-300 before:fixed before:top-0 before:left-0 before:w-full before:h-full before:backdrop-blur-sm before:bg-black/10 max-w-[400px]`}>
             <div className="hiddden"></div>
-            <div className={`${showOptions ? '' : 'scale-50 opacity-0 pointer-events-none'} ${isCloserToLeft ? 'origin-top-left translate-x-[260px]' : 'origin-top-right'} transition-[transform,opacity] duration-300 absolute w-[240px] top-0 right-0 bg-white rounded-[20px] p-3 z-[9999]`}>
+            <div className={`${showOptions ? '' : 'scale-50 opacity-0 pointer-events-none'} ${isCloserToLeft ? 'origin-top-left md:translate-x-[260px]' : 'md:origin-top-right origin-top'} transition-[transform,opacity] duration-300 absolute w-full md:w-[240px] top-0 right-0 bg-white rounded-[20px] p-3 z-[9999]`}>
                 <ul className={`${showOptions ? 'delay-100 duration-300' : 'opacity-0'} transition-opacity flex flex-col`}>
                     {
                         OPTIONS.map(({ title, action, hoverBackground, hoverColor, Icon }, index) => (
@@ -62,7 +68,7 @@ export default function TextSpaceCard({ id, index = 0 }: { id: number | string; 
                 </ul>
             </div>
 
-            <Link to={`/spaces/space/${id}`} style={{ '--delay': `${(Number(index) * 100) + 200}ms` } as React.CSSProperties} onClick={handleClick} className={`${showOptions ? !isCloserToLeft && 'translate-x-[-260px]' : ''} animated-card opacity-0 transition-transform duration-300 relative p-4 flex flex-col gap-4 aspect-square bg-white rounded-[30px] shadow-sm shadow-black/10 justify-between`}>
+            <Link to={`/spaces/space/${id}`} style={{ '--delay': `${(Number(index) * 100) + 200}ms` } as React.CSSProperties} onClick={handleClick} className={`${showOptions ? `${!isCloserToLeft && 'md:translate-x-[-260px]'} translate-y-[210px] md:translate-y-0` : ''} animated-card opacity-0 transition-transform duration-300 relative p-4 flex flex-col gap-4 aspect-square bg-white rounded-[30px] shadow-sm shadow-black/10 justify-between`}>
                 <div className="flex justify-between items-center gap-3">
                     <div className="flex flex-col">
                         <h3 className="font-bold text-lg">Space title</h3>
@@ -76,14 +82,14 @@ export default function TextSpaceCard({ id, index = 0 }: { id: number | string; 
                         </span>
                     </button>
                 </div>
-                <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center justify-between gap-2 flex-wrap">
                     <span className="flex items-center gap-2 rounded-full">
                         <span className="flex items-center justify-center h-[26px] aspect-square rounded-full bg-black text-white">
-                            <MdPerson size={18} />
+                            <MdPerson size={20} />
                         </span>
                         <span className='text-sm'>Anonymous</span>
                     </span>
-                    <div className="flex items-center gap-1 text-gray-400">
+                    <div className="flex items-center text-gray-400">
                         <span className="font-semibold text-sm">10 views</span>
                         <BsDot size={16} />
                         <span className="font-semibold text-sm">1 min.</span>
