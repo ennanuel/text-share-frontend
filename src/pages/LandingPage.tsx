0151aae84670cc392f874bc289a0a2ec37e4970a
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-09-09 16:58:56 +0100

    Made page responsive

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,27 +6,26 @@ import { FaAngleRight } from "react-icons/fa6";
 import { FiCopy, FiLink, FiLock } from "react-icons/fi";
 import { HiOutlineDocument } from "react-icons/hi2";
 import { MdAdd, MdClose, MdMoreHoriz, MdPerson } from "react-icons/md";
-import { SiExpress, SiMongodb } from "react-icons/si";
 
 
 export default function LandingPage() {
     return (
         <div id="landing-page">
-            <section className="relative min-h-[100vh] flex gap-20 items-center justify-center px-[10%] overflow-x-clip overflow-y-visible">
-                <div className="flex-1 flex flex-col gap-8">
+            <section className="relative min-h-[100vh] flex gap-20 items-center justify-center px-6 md:px-[10%] overflow-x-clip overflow-y-visible">
+                <div className="relative z-[1] flex-1 flex flex-col gap-8">
                     <h1 className="text-[4rem] leading-[4.8rem] font-semibold">Share text.<br />Built for privacy</h1>
                     <p className="text-xl text-gray-600">Build, deploy, and scale your apps with unparalleled ease - from your first user to your billionth.</p>
                     <div className="flex items-center gap-6 mt-6">
-                        <button className="group relative h-[60px] min-w-[200px] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:rounded-full before:bg-gradient-to-br before:from-blue-400 before:to-purple-800 before:blur-xl before:transition-transform before:scale-x-90 before:scale-y-75 before:duration-300 hover:before:scale-110">
+                        <button className="group relative h-[60px] md:min-w-[200px] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:rounded-full before:bg-gradient-to-br before:from-blue-400 before:to-purple-800 before:blur-xl before:transition-transform before:scale-x-90 before:scale-y-75 before:duration-300 hover:before:scale-110">
                             <span className="relative h-full rounded-full bg-white px-8 flex gap-3 items-center justify-center font-semibold group-hover:text-blue-800 transition-colors">Get Started</span>
                         </button>
-                        <button className="group relative h-[60px] min-w-[200px] text-white rounded-full flex items-center justify-center gap-3 px-8 before:absolute before:top-0 before:left-0 before:w-full before:h-full before:rounded-full before:bg-black before:transition-transform hover:before:scale-110">
+                        <button className="group relative h-[60px] md:min-w-[200px] text-white rounded-full flex items-center justify-center gap-3 px-8 before:absolute before:top-0 before:left-0 before:w-full before:h-full before:rounded-full before:bg-black before:transition-transform hover:before:scale-110">
                             <span className="font-semibold relative">Sign up</span>
                             <AiOutlineArrowRight size={20} className="relative group-hover:translate-x-2 transition-transform" />
                         </button>
                     </div>
                 </div>
-                <div className="flex-1 relative flex items-end">
+                <div className="flex-1 absolute bottom-0 right-0 translate-y-[-100%] opacity-50 md:opacity-100 md:translate-y-0 md:relative flex items-end">
                     <div className="absolute top-[50%] left-[80%] translate-x-[-50%] translate-y-[-70%] before:absolute before:bottom-[-10%] before:left-[-5%] before:h-full before:aspect-square before:rounded-full before:bg-gradient-to-br before:from-pink-400 before:to-purple-600 before:blur-xl before:opacity-50">
                         <div className="relative flex flex-col border border-gray-300 rounded-[20px] overflow-hidden shadow-lg shadow-blue-600/20 ">
                             <div className="bg-gray-800 flex items-center gap-3 px-4 py-3 border-b border-gray-300">
@@ -111,26 +110,26 @@ export default function LandingPage() {
                 <section className="relative z-[1] translate-y-[-50%] flex items-center">
                     <span className="block w-[20%] h-[160px] bg-black rounded-full"></span>
                     <ul className=" flex-1 w-[60%] flex gap-6 justify-evenly p-[5%] bg-[#efefef] rounded-full">
-                        <li className="flex flex-1 flex-col text-center gap-4">
+                        <li className="hidden md:flex flex-1 flex-col text-center gap-4">
                             <h3 className="text-xl font-bold">49K+</h3>
                             <p className="text-sm text-gray-600">Customers</p>
                         </li>
-                        <li className="flex flex-1 flex-col text-center gap-4">
+                        <li className="hidden md:flex flex-1 flex-col text-center gap-4">
                             <h3 className="text-xl font-bold">115+</h3>
                             <p className="text-sm text-gray-600">Regions across AWS, Azure, and Google Cloud</p>
                         </li>
-                        <li className="flex flex-1 flex-col text-center gap-4">
+                        <li className="hidden md:flex flex-1 flex-col text-center gap-4">
                             <h3 className="text-xl font-bold">175K+</h3>
                             <p className="text-sm text-gray-600">Developers join every month</p>
                         </li>
-                        <li className="flex flex-1 flex-col text-center gap-4">
+                        <li className="hidden md:flex flex-1 flex-col text-center gap-4">
                             <h3 className="text-xl font-bold">#1</h3>
                             <p className="text-sm text-gray-600">Most used modern database</p>
                         </li>
                     </ul>
                     <span className="block w-[20%] h-[160px] bg-gradient-to-b from-[60%] from-black to-[60%] to-transparent rounded-l-[100px]"></span>
                 </section>
-                <section className="relative flex flex-col gap-20 items-center justify-center p-[10%] pt-0 text-white">
+                <section className="relative flex flex-col gap-20 items-center justify-center p-6 md:p-[10%] mb-[100px] md:mb-0 pt-0 text-white">
                     <div className="absolute bottom-0 right-0 translate-x-[35%] translate-y-[8%] h-[130vh] aspect-square flex-items-center justify-center rounded-full overflow-hidden">
                         <div className="absolute top-0 left-0 w-full h-full rounded-full bg-gradient-to-br from-purple-600 to-blue-900"></div>
                         <div className="absolute top-[42%] left-[42%] w-[80%] aspect-square rotate-45 translate-x-[-50%] translate-y-[-50%] rounded-full border-l-[40px] border-yellow-400 blur-xl"></div>          
@@ -141,8 +140,8 @@ export default function LandingPage() {
                         <div className="absolute top-[50%] left-[55%] w-[80%] aspect-square rotate-45 translate-x-[-50%] translate-y-[-50%] rounded-full border-r-[25px] border-yellow-400 blur-xl"></div>          
                         <div className="absolute top-[50%] left-[50%] w-[80%] translate-x-[-50%] translate-y-[-50%] aspect-square rounded-full bg-black"></div>
                     </div>
-                    <div className="flex w-full justify-between items-center gap-4 relative">
-                        <h2 className="text-4xl font-semibold">Unlock the power of AI with MongoDB</h2>
+                    <div className="flex flex-col md:flex-row w-full justify-center md:justify-between items-center gap-10 md:gap-4 relative">
+                        <h2 className="text-4xl font-semibold text-center md:text-start">Unlock the power of AI with MongoDB</h2>
                         <a href="#" className="relative before:absolute before:top-0 before:left-0 before:w-full before:h-full before:rounded-full before:bg-gradient-to-br before:from-blue-600 before:to-purple-800 before:blur-xl before:scale-y-75 before:scale-x-90">
                             <span className="flex relative items-center justify-center px-6 gap-3 h-[50px] rounded-full bg-black">
                                 <span className="text-sm font-semibold">Try now</span>
@@ -150,7 +149,7 @@ export default function LandingPage() {
                             </span>
                         </a>
                     </div>
-                    <ul className="relative flex gap-6">
+                    <ul className="relative grid grid-cols-1 md:grid-cols-3 gap-6">
                         <li className="flex-1 backdrop-blur-xl flex flex-col justify-center items-center text-center gap-10 p-6 aspect-square rounded-[30px] bg-gradient-to-br from-white/5 to-white/10">
                             <span className="bg-black flex items-center justify-center w-[60px] h-[60px] rounded-[16px]">
                                 <HiOutlineDocument size={30} />
@@ -180,12 +179,12 @@ export default function LandingPage() {
                         </li>
                     </ul>
                 </section>
-            <section className="bg-black text-white flex flex-col items-center justify-center gap-20 p-[10%]">
+            <section className="bg-black text-white flex flex-col items-center justify-center gap-20 p-6 md:p-[10%]">
                 <div className="flex flex-col gap-3 relative">
                     <h2 className="text-[3rem] font-semibold text-center">Built by developers, for developers</h2>
                     <h3 className="text-2xl font-semibold text-gray-400 text-center">The document data model maps to how you think and code.</h3>
                 </div>
-                <div className="flex relative">
+                <div className="flex flex-col gap-20 md:gap-4 md:flex-row relative">
                     <ul className="flex-1 flex flex-col gap-6">
                         <li className="relative flex flex-col gap-6 py-2 px-8 before:absolute before:top-0 before:left-0 before:w-[3px] before:h-full before:bg-gray-400">
                             <h3 className="text-3xl font-semibold">Model</h3>
@@ -233,7 +232,7 @@ export default function LandingPage() {
                     </div>
                 </div>
                 </section>
-                <section className="p-[5%] relative rounded-[100px] rounded-tl-none bg-[#efefef] flex flex-col gap-20 overflow-clip">
+                <section className="p-6 py-[100px] md:py-[5%] md:p-[5%] relative rounded-[100px] rounded-tl-none bg-[#efefef] flex flex-col gap-20 overflow-clip">
                     <div className="absolute top-0 right-0 translate-x-[20%] translate-y-[20%] h-[130vh] aspect-square flex-items-center justify-center rounded-full overflow-hidden">
                         <div className="absolute top-0 left-0 w-full h-full rounded-full bg-gradient-to-br from-purple-400 to-blue-600"></div>
                         <div className="absolute top-[42%] left-[42%] w-[80%] aspect-square rotate-45 translate-x-[-50%] translate-y-[-50%] rounded-full border-l-[30px] border-yellow-300 blur-xl"></div>          
@@ -242,14 +241,14 @@ export default function LandingPage() {
                     <div className="relative flex justify-between gap-10">
                         <div className="flex flex-col">
                             <h2 className="text-[3rem] font-semibold">Still not sold?</h2>
-                            <h3 className="text-3xl font-semibold text-gray-400">Check out what you'll be getting in on.</h3>
+                            <h3 className="text-2xl md:text-3xl font-semibold text-gray-400">Check out what you'll be getting in on.</h3>
                         </div>
-                        <button className="group relative flex items-center justify-center gap-4 px-8 hover:text-blue-600">
+                        <button className="group relative hidden md:flex items-center justify-center gap-4 px-8 hover:text-blue-600">
                             <text className="relative text-lg font-semibold">Try out now</text>
                             <FaAngleRight size={20} className="relative transition-transform group-hover:translate-x-2" />
                         </button>
                     </div>
-                    <ul className="flex items-center justify-center gap-10">
+                    <ul className="grid grid-cols-1 md:grid-cols-3 gap-10">
                         <li className="flex-1 flex flex-col justify-between gap-4 p-8 aspect-square rounded-[30px] border border-gray-400 backdrop-blur-xl">
                             <span className="w-[60px] h-[60px] rounded-[20px] bg-white flex items-center justify-center">
                                 <FiLock size={20} />
