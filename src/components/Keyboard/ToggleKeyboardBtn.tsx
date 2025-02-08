06f4618f4449af70e670570925cefbe157ea7233
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-08 11:01:11 +0100

    Added design for dark mode

--- src/components/Keyboard/ToggleKeyboardBtn.tsx
+++ src/components/Keyboard/ToggleKeyboardBtn.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { FaKeyboard } from "react-icons/fa";
 import { keyboardContext } from ".";
 
-export default function ToggleKeyboardButton({ inputId }: { inputId: string; }) {
+export default function ToggleKeyboardButton({ inputId, darkMode }: { inputId: string; darkMode?: boolean }) {
     const { showKeyboard, idOfInputInFocus, openKeyboard, closeKeyboard } = useContext(keyboardContext);
 
     return (
@@ -10,7 +10,7 @@ export default function ToggleKeyboardButton({ inputId }: { inputId: string; }) 
             title="Toggle virtual keyboard"
             htmlFor={inputId}
             onClick={() => showKeyboard && inputId === idOfInputInFocus ? closeKeyboard() : openKeyboard(inputId)}
-            className={`flex cursor-pointer items-center justify-center aspect-square rounded-full ${showKeyboard && idOfInputInFocus === inputId ? 'bg-blue-100 text-blue-600 hover:bg-gray-100 hover:text-gray-600 showing-keyboard' : 'text-gray-600 hover:bg-blue-100 hover:text-blue-600'} min-w-[30px]`}
+            className={`flex cursor-pointer items-center justify-center aspect-square rounded-full ${showKeyboard && idOfInputInFocus === inputId ? `${darkMode ? 'bg-blue-400/20 text-blue-400 hover:bg-white/10 hover:text-gray-200 ' : 'bg-blue-100 text-blue-600 hover:bg-gray-100 hover:text-gray-600 '} showing-keyboard` : darkMode ? `text-white/70 hover:bg-white/10 hover:text-white` :  `text-gray-600 hover:bg-blue-100 hover:text-blue-600`} min-w-[30px]`}
         >
             <FaKeyboard size={16} />
         </label>
