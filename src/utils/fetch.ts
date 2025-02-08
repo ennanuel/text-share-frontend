4cf7b20eafde6dbb3838e0b876173a8c5b7ed928
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-08 10:43:05 +0100

    Added refetch logic to function, and changed error logic

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -4,36 +4,43 @@ import { ModifiedError } from "../types/error.type";
 
 export function useFetch<Data>(url: string, options: RequestInit): {
     loading: boolean;
-    failed: boolean;
+    error: { statusCode?: number, message: string; } | null;
     data: Data | null;
+    retry: () => void;
 } {
     const [loading, setLoading] = useState(true);
-    const [failed, setFailed] = useState(false);
+    const [error, setError] = useState<{ statusCode?: number; message: string; } | null>(null);
     const [data, setData] = useState<Data|null>(null);
+    const [refetch, setRefetch] = useState(0);
+
+    const retry = () => setRefetch(refetch + 1);
 
     useEffect(() => {
         setLoading(true);
-        setFailed(false);
+        setError(null);
 
         fetch(url, options)
             .then((response) => {
                 response
                     .json()
                     .then((result) => {
-                        if (response.status !== 200) throw result;
+                        if (response.status !== 200) {
+                            setError({ statusCode: response.status, message: "" });
+                            throw result;
+                        }
                         setData(result)
                     })
                     .catch((error) => { 
                         console.error((error as ModifiedError).message);
-                        setFailed(true);
+                        setError({ statusCode: response.status, message: (error as ModifiedError).message });
                     })
             })
             .catch((error) => {
                 console.error((error as ModifiedError).message);
-                setFailed(true);
+                setError((prev) => ({ ...prev, message: (error as ModifiedError).message }));
             })
             .finally(() => setLoading(false));
-    }, [url]);
+    }, [url, refetch]);
 
-    return { loading, failed, data };
+    return { loading, error, data, retry };
 }
