336499f0cda92fa4fee115d49b0390654d6194ee
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-08 10:44:29 +0100

    Added color field to 'TextSpace' type

--- src/types/textSpace.type.ts
+++ src/types/textSpace.type.ts
@@ -10,9 +10,18 @@ export interface TextSpace {
     secured: boolean;
     views: number;
     createdAt: string;
+    color: string;
     owner: {
         _id: string;
         username: string;
         profileImage?: string;
     }
+};
+
+export type TextSpacesFetchResult = {
+    page: number;
+    limit: number;
+    sortedBy: string;
+    totalPages: number;
+    textSpaces: TextSpace[];
 }
