85b259e968953471b1276056d63db2a698b87c2b
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-08 10:49:16 +0100

    made some design changes, and added logic for editting, and unlocking secured text spaces

--- src/pages/TextSpace.tsx
+++ src/pages/TextSpace.tsx
@@ -1,96 +1,98 @@
 
-import { Link } from "react-router-dom";
+import { useEffect, useMemo, useState } from "react";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 
-import { AiOutlineArrowLeft } from "react-icons/ai";
-import { BiCopy } from "react-icons/bi";
-import { BsDot } from "react-icons/bs";
-import { FaAngleDown } from "react-icons/fa";
-import { FiClock, FiCopy } from "react-icons/fi";
-import { IoLink } from "react-icons/io5";
 import { LuMoveLeft } from "react-icons/lu";
-import { MdPerson } from "react-icons/md";
 
-import OptionsModal from "../components/OptionsModal";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
 
-import ToggleKeyboardButton from "../components/Keyboard/ToggleKeyboardBtn";
+import { useFetch } from "../utils/fetch";
+import { fetchOptions } from "../assets/data";
+
+import { TextSpace } from "../types/textSpace.type";
+import UnlockSecuredSpace from "../components/UnlockSecuredSpace";
+import EditTextSpace from "../components/EditTextSpace";
+import NormalTextSpace from "../components/NormalTextSpace";
+import { socket } from "../App";
 
 
-export default function TextSpace() { 
+export default function TextSpacePage() { 
+    const { id } = useParams();
+    const [copied, setCopied] = useState({ link: false, content: false });
+
+    const [url, setUrl] = useState(`${import.meta.env.VITE_SERVER_URL}/spaces/space/${id}`);
+
+    const { loading, error, data, retry } = useFetch<TextSpace>(url, fetchOptions);
+    const authenticationFailed = useMemo(() => error?.statusCode === 401, [error]);
+
+    const [password, setPassword] = useState("");
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const editMode = useMemo(() => searchParams.get('edit') === 'true', [searchParams]);
+
+    const enterEditMode = () => setSearchParams({ edit: 'true' });
+    const exitEditMode = () => setSearchParams({ });
+
+    const submitPassword: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+        setUrl(`${import.meta.env.VITE_SERVER_URL}/spaces/space/${id}?p=${password}`);
+        setPassword("");
+    };
+
+    useEffect(() => { 
+        socket.on("editted", ({ textSpaceId }: { textSpaceId: string }) => {
+            if(textSpaceId === id) retry();
+        })
+    }, [socket]);
+
+    if(loading) return (
+        <div className="min-h-screen flex items-center justify-center">
+            <Loading text="Loading text space..." />
+        </div>
+    );
+    else if(error && !authenticationFailed) return (
+        <div className="min-h-screen flex item-center justify-center">
+            <Error />
+        </div>
+    );
+    else if(!data && !authenticationFailed) return (
+        <div className="min-h-screen flex items-center justify-center">
+            <span>Nothing was found!</span>
+        </div>
+    );
+
     return (
-        <body className="flex flex-col md:flex-row gap-8 min-h-[calc(100vh_-_60px)] relative mt-10 px-6 md:px-[10vw]">
-            <div className="flex-1 flex-col justify-between gap-10">
-                <div className="flex flex-col gap-10">
-                    <Link to="/explore" className="group flex items-center gap-2">
+        <div className="flex flex-col gap-10 mt-10 px-4">
+            <UnlockSecuredSpace 
+                setPassword={setPassword} 
+                submitPassword={submitPassword}
+                authenticationFailed={authenticationFailed} 
+            />
+            {
+                editMode ?
+                    <button onClick={exitEditMode} type="button" className="group hover:text-red-500 flex items-center gap-2">
+                        <LuMoveLeft size={18} className="relative text-inherit transition-[transform,_color] duration-300 group-hover:-translate-x-2" />
+                        <span className="font-semibold relative text-inherit transition-colors before:absolute before:bottom-0 before:w-full before:h-[2px] before:bg-red-500 before:scale-x-0 before:transition-transform before:duration-200 before:origin-left group-hover:before:scale-x-100">Back to normal</span>
+                    </button> :
+                    <Link to="/spaces" className="group flex items-center gap-2">
                         <LuMoveLeft size={18} className="relative transition-transform duration-300 group-hover:-translate-x-2" />
                         <span className="font-semibold relative before:absolute before:bottom-0 before:w-full before:h-[2px] before:bg-black before:scale-x-0 before:transition-transform before:duration-200 before:origin-left group-hover:before:scale-x-100">Text spaces</span>
                     </Link>
-                    <div className="flex items-center justify-between gap-4">
-                        <h1 className="text-[2rem] font-bold flex-1">Page title</h1>
-                        <OptionsModal />
-                    </div>
-                    <ul className="flex flex-wrap items-center gap-2">
-                        <li className="font-semibold flex items-center gap-2 h-[35px] rounded-full bg-white pl-[5px] pr-3">
-                            <span className="flex items-center justify-center h-[25px] w-[25px] rounded-full bg-black text-white">
-                                <MdPerson size={16} />
-                            </span>
-                            <span className="text-sm font-semibold">Anonymous</span>
-                        </li>
-                        <li className="h-[35px] rounded-full bg-white text-sm font-semibold flex gap-2 items-center justify-center pl-[5px] pr-3">
-                            <span className="flex items-center justify-center h-[25px] w-[25px] rounded-full bg-black text-white">
-                                <FiClock size={16} />
-                            </span>
-                            <span>20 mins. ago</span>
-                        </li>
-                        <li className="h-[35px] rounded-full bg-white text-sm font-semibold flex flex-row-reverse gap-2 items-center justify-center pl-[5px] pr-3">
-                            <span>10 views</span>
-                            {
-                                [1, 2, 3, 4, 5].map((item, index, arr) => (
-                                    <span key={index} className={`relative flex ${index == arr.length - 1 ? '' : 'ml-[-20px]'} items-center justify-center h-[23px] w-[23px] rounded-full bg-gray-200 border-2 border-white`}>
-                                        <MdPerson size={16} />
-                                    </span>
-                                ))
-                            }
-                        </li>
-                    </ul>
-                    <p className="text-gray-400">Visit the website on two devices or browser windows <br />Enter your text in the field to share.</p>
-                    <div className="flex items-center gap-4 bg-white rounded-full p-2 border border-gray-200">
-                        <input type="url" className="flex-1 h-[45px] text-sm bg-transparent px-4" placeholder="https://google.com" />
-                        <button className="relative w-[45px] aspect-square bg-black text-white hover:bg-black/90 rounded-full flex items-center justify-center gap-2">
-                            <FiCopy size={20} />
-                        </button>
-                    </div>
-                </div>
-                <div></div>
-            </div>
-            <div className="flex-1 flex flex-col gap-4">
-                <div className="relative">
-                    <textarea name="" id="text-space-area" className="w-full min-h-[60vh] rounded-[30px] ng-white shadow-lg shadow-black/10 p-6" placeholder="Text that is in this space..."></textarea>
-                    <div className="absolute bottom-4 right-4">
-                        <ToggleKeyboardButton inputId="text-space-area" />
-                    </div>
-                </div>
-                <div className="flex justify-between items-center">
-                    <ul className="text-gray-400 flex items-center flex-wrap">
-                        <li className="flex items-center justify-center gap-2">
-                            <span className="text-sm font-semibold"><span className="text-gray-600">50</span> / 250 words</span>
-                        </li>
-                        <BsDot size={24} />
-                        <li className="flex items-center justify-center gap-2">
-                            <span className="text-sm font-semibold"><span className="text-gray-600">140</span> / 2600 chars.</span>
-                        </li>
-                    </ul>
-                    <button className="flex items-center justify-center gap-2 text-black px-2">
-                        <span className="text-sm font-semibold">10 links</span>
-                        <FaAngleDown size={12} />
-                    </button>
-                </div>
-                <div className="flex items-center gap-4 mt-2">
-                    <button className="btn group relative flex items-center justify-center gap-3 w-[90%] m-auto">
-                        <span className="font-semibold relative text-sm">Copy to clipboard</span>
-                        <BiCopy size={18} className="relative group-hover:scale-110 transition-transform" />
-                    </button>
-                </div>
-            </div>
-        </body>
+
+            }
+            {
+                editMode ?
+                    <EditTextSpace textSpace={data} /> :
+                    <NormalTextSpace 
+                        textSpace={data} 
+                        copied={copied} 
+                        enterEditMode={enterEditMode} 
+                        copyLink={() => null} 
+                        share={() => null} 
+                        remove={() => null} 
+                    />
+            }
+        </div>
     )
 };
