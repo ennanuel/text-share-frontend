23a1ad73ecf3be9e7cce4b68fdf338d6ee770087
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-18 13:44:47 +0100

    Made page responsive

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,9 +13,9 @@ export default function About() {
     
     return (
         <div className="min-h-screen flex flex-col gap-6 max-w-[var(--max-width)] my-auto p-4 pt-20 md:pt-[80px] pb-20">
-             <div className="w-full flex items-end justify-between p-4 pt-6 sm:pt-12 sm:p-8 rounded-[32px] bg-[#303030] tracking-tight">
+             <div className="w-full flex flex-col md:flex-row gap-6 md:items-end md:justify-between p-4 pt-6 sm:pt-12 sm:p-8 rounded-[32px] bg-[#303030] tracking-tight">
                 <div className="flex flex-1 flex-col gap-12">
-                    <h2 className="text-white text-4xl md:text-[3rem] lg:text-[4rem] font-bold animate-[fade-in_1s_ease]">About</h2>
+                    <h2 className="text-white text-2xl md:text-4xl md:text-[3rem] lg:text-[4rem] font-bold animate-[fade-in_1s_ease]">About</h2>
                     <div className="flex gap-6">
                         <p className="flex-1 max-w-[80ch] text-white/60 text-base md:text-lg">
                             <span className="animate-[fade-in_1s_ease] delay-100">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi impedit consectetur provident ad dolore blanditiis cupiditate sint fugiat fuga, assumenda ipsum ducimus odit? Minus delectus soluta dicta facilis, quas earum.</span>
@@ -24,12 +24,12 @@ export default function About() {
                             <span className="animate-[fade-in_1s_ease] delay-200">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi impedit consectetur provident ad dolore blanditiis cupiditate sint fugiat fuga, assumenda ipsum ducimus odit? Minus delectus soluta dicta facilis, quas earum.</span>
                         </p>
                     </div>
-                    <div className="flex gap-2 mt-8">
-                        <button className="rounded-full relative flex items-center justify-center gap-2 min-w-40 px-6 h-12 border border-white/20 text-white/80 hover:bg-white/10 hover:border-transparent">
+                    <div className="flex flex-wrap gap-2 mt-8">
+                        <button className="rounded-full relative flex items-center justify-center gap-2 flex-1 md:flex-auto sm:min-w-40 px-4 md:px-6 h-12 border border-white/20 text-white/80 hover:bg-white/10 hover:border-transparent">
                             <BiMessage size={18} />
                             <span className="relative font-semibold text-sm md:text-base">Let's chat</span>
                         </button>
-                        <button className="bg-white text-black rounded-full relative flex items-center justify-center gap-2 min-w-40 px-6 h-12 border border-white">
+                        <button className="bg-white text-black rounded-full relative flex items-center flex-1 md:flex-auto justify-center gap-2 sm:min-w-40 px-4 md:px-6 h-12 border border-white">
                             <FiGithub size={18} className="relative" />
                             <span className="relative font-semibold text-sm md:text-base">View code</span>
                         </button>
