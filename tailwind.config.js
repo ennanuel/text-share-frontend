8d578218b5ff5fbb9d1ac8deafdf3bdaa1d50136
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-18 03:22:30 +0100

    Added some configuration fields

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./src/**/*.{html,js,ts,tsx,css}"],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: "'Montserrat'"
+      },
+      fontWeight: {
+        normal: 500
+      },
       transitionTimingFunction: {
         expo: 'cubic-bezier(0.16, 1, 0.3, 1)'
       }
