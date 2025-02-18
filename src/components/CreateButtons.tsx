717ac92aa389a3b0b8d8cfb68ad25113ca05f9e6
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-18 03:47:11 +0100

    made some minor changes

--- src/components/CreateButtons.tsx
+++ src/components/CreateButtons.tsx
@@ -20,7 +20,7 @@ export default function CreateButtons() {
     if (pathname.includes('landing')) return;
 
     return (
-        <div className="flex justify-end sticky bottom-6 h-0 px-4 mt-10 mb-6 w-full max-w-[var(--big-max-width)]">
+        <div className="flex justify-end sticky bottom-2 h-0 px-4 mt-10 w-full max-w-[var(--big-max-width)]">
             <button onClick={open} className={`group relative -top-14 text-white flex items-center justify-center h-12 md:h-14 pl-6 pr-4 before:absolute before:w-full before:h-full before:rounded-full before:bg-black/80 before:backdrop-blur hover:before:scale-110 before:transition-transform before:border before:border-white/10`}>
                 <span className="relative inline-flex items-center justify-center gap-2">
                     <span className='text-sm font-semibold'>New space</span>
